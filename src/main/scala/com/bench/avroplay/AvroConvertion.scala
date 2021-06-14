86b00c7b2a84a2788df13dbf4b87d5f9cdba09b4
brokenpath <brokenpath@nowhere.com>
2021-06-14 23:56:34 +0200

    outline a possible solution

--- src/main/scala/com/bench/avroplay/AvroConvertion.scala
+++ src/main/scala/com/bench/avroplay/AvroConvertion.scala
@@ -8,6 +8,8 @@ import org.apache.avro.specific.SpecificRecordBase
 import collection.JavaConverters._
 import org.apache.avro.specific.SpecificDatumReader
 import tryllerylle.benchrows
+import org.apache.spark.rdd.RDD
+import org.apache.spark.input.PortableDataStream
 
 object Main extends App {
     val (inputFile, outputFile) = (args(0), args(1))
@@ -15,8 +17,24 @@ object Main extends App {
 }
 
 
+case class AvroFileGroups()
+
 object Runner {
     val HADOOP_BLOCK_SIZE = 128 * 1024 * 1024  // improvement get dynamically from hdfs
+
+
+
+
+    /*
+        Map to vector and reduceByKey. 
+        PortableDataStream is unopened we need to somehow do a foreach with a sideeffect open the file
+        write loop though each datastream and close it.
+        
+    */
+    def mergeAvro(rdd: RDD[(String, (String, PortableDataStream))]) : Unit = {
+        
+        
+    }
 
     def run(conf: SparkConf, path: String, outputFolder: String): Unit = {
         val sc = new SparkContext(conf)
